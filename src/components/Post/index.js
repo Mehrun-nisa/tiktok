3cb580092b555cb16c2534336016a55bbf8a2179
Mehrunnisa123 <mehrunnisa562@gmail.com>
2022-08-30 21:34:32 +0500

    Adding Pause/Play

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,17 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableWithoutFeedback } from 'react-native'
+import React, { useState } from 'react'
 import Video from 'react-native-video'
 import styles from './styles'
 
 const Post = () => {
+    const [paused, setPaused] = useState(false);
+
+    const onPlayPausePress =()=>
+    {
+        setPaused(!paused);
+    };
   return (
     <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={onPlayPausePress}>
     <Video 
     source={{uri:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4'}} 
      style={styles.video}
      resizeMode={'cover'}
      repeat={true}
+     paused={paused}
     />
+    </TouchableWithoutFeedback>
  </View>
     )
 }
